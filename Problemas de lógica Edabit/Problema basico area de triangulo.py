13cd680442e0e6385c0aeff20d9624a999fa3a15
matehusu <matehusu@gmail.com>
2023-12-10 22:19:31 -0300

    Problemas de python basicos

---
+++ Problemas de lógica Edabit/Problema basico area de triangulo.py
@@ -0,0 +1,36 @@
+#Problemas básicos de python
+#-----------------------------------------------------
+"""def tri_area(base, height):
+    area = (base * height) / 2
+    return area
+base= 7
+height=7
+
+result_area = tri_area(base, height)
+
+print (result_area)
+#------------------------------------------------------
+def remainder(x, y):
+	
+    resultado = x % y
+    return resultado
+
+x=1
+y=2
+resultado= remainder(x,y)
+resultado=round(resultado)
+print(resultado)
+
+def addition(a, b):
+    x= a + b
+    return x
+"""
+#------------------------------------------------------
+def string_int(txt):
+    txt = int(txt)
+    return txt
+
+txt_str= "90"
+txt_int = string_int(txt_str)
+txt_int = txt_int + 4
+print(txt_int)
